557446df03f38e0144c793fc40e979fa97f5b38e
D I Y A <116140618+diyatalsaniya@users.noreply.github.com>
2024-09-09 11:31:55 +0530

    Add files via upload

---
+++ Programming/Class Work/C/9-8-24/Switch Case.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+main()
+{
+	char n;
+	
+	printf("Enter Character : ");
+	scanf("%c",&n);
+	
+	switch(n)
+	{
+		case 'a':
+			printf("%c is Vowel",n);
+			break;
+		
+		case 'A':
+			printf("%c is Vowel",n);
+			break;
+			
+		case 'e':
+			printf("%c is Vowel",n);
+			break;
+			
+		case 'E':
+			printf("%c is Vowel",n);
+			break;
+			
+		case 'i':
+			printf("%c is Vowel",n);
+			break;
+			
+		case 'I':
+			printf("%c is Vowel",n);
+			break;
+			
+		case 'o':
+			printf("%c is Vowel",n);
+			break;
+			
+		case 'O':
+			printf("%c is Vowel",n);
+			break;	
+			
+		case 'u':
+			printf("%c is Vowel",n);
+			break;
+			
+		case 'U':
+			printf("%c is Vowel",n);
+			break;
+			
+		default:
+			{
+				printf("%c is Consonant",n);
+				break;
+			}
+			
+	}
+}
